8c02fca0f18f04b1786edeffad6af9497128c5b1
OliverJ-LCWC <144791858+OliverJ-LCWC@users.noreply.github.com>
2024-12-03 14:27:41 +0000

    woo

--- employee.py
+++ employee.py
@@ -1,8 +1,8 @@
 class Employee:
     def __init__(self, firstname, lastname) -> None:
-        self.__firstname = firstname
-        self.__lastname = lastname
-        self.__email = f"{firstname}.{lastname}@company.com"
-        fullname = f"{firstname} {lastname}"
+        self._firstname = firstname
+        self.lastname = lastname
+        self.email = f"{firstname}.{lastname}@company.com"
+        self.fullname = f"{firstname} {lastname}"
 
 
